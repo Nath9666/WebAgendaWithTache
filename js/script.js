78423382c6e59ec7c00dc4d5fc8fc9c656ce0b9f
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-04-07 11:24:54 +0200

    prettier version

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-    var header = document.getElementById("header")
-    header.innerHTML = '<h1>Calentask</h1>    \
+window.addEventListener("DOMContentLoaded", () => {
+  var header = document.getElementById("header")
+  header.innerHTML =
+    '<h1>Calentask</h1>    \
                         <nav>        \
-                            <a href="Accueil.html"><h3>Accueil</h3></a>    \
+                            <a href="Accueil.html"><h3>Accuei</h3></a>    \
                             <a href="Mon_agenda.html"><h3>Mon agenda</h3></a>\
                             <a href="Ajouter.html"><h3>Ajouter</h3></a>     \
                             <a href="A_propos.html"><h3>À propos</h3></a> \
                         </nav>      \
                         <button id="button2">Dark Mode</button>'
-    console.log("La page est chargée")
-    var button2 = document.getElementById("button2");
+  console.log("La page est chargée")
+  var button2 = document.getElementById("button2")
 
-    var footer = document.getElementById("footer")
-    footer.innerHTML = '<div class="reseau">    \
+  var footer = document.getElementById("footer")
+  footer.innerHTML =
+    '<div class="reseau">    \
                         <a href="https://fr-fr.facebook.com" target="_blank"> \
                             <img src="../images/logo_facebook.jpg" alt=""> \
                             <span>Facebook</span>    \
@@ -31,16 +33,14 @@ window.addEventListener('DOMContentLoaded', () => {
                         </a> \
                     </div>'
 
-
-    button2.addEventListener('click', () => {
-        document.body.classList.toggle("dark_theme")
-        if (button2.innerHTML == 'Dark Mode') {
-            button2.innerHTML = 'Light Mode'
-            document.getElementById("ht").style.backgroundColor = "black"  
-        } else {
-            button2.innerHTML = 'Dark Mode'
-            document.getElementById("ht").style.backgroundColor = "white"
-        }
-    })
+  button2.addEventListener("click", () => {
+    document.body.classList.toggle("dark_theme")
+    if (button2.innerHTML == "Dark Mode") {
+      button2.innerHTML = "Light Mode"
+      document.getElementById("ht").style.backgroundColor = "black"
+    } else {
+      button2.innerHTML = "Dark Mode"
+      document.getElementById("ht").style.backgroundColor = "white"
+    }
+  })
 })
-
